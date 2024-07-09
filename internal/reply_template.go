4d779629b29dedd4f3e477d21ad16a96e2f2bf89
islu <38793955+islu@users.noreply.github.com>
2024-07-09 22:48:55 +0800

    fix: error

--- internal/reply_template.go
+++ internal/reply_template.go
@@ -25,7 +25,8 @@ func ModeSettingTemplateV2(liffID string) messaging_api.MessageInterface {
 	return &messaging_api.TemplateMessage{
 		AltText: "Setting role alt text",
 		Template: &messaging_api.ButtonsTemplate{
-			Title: "設定角色",
+			Title: "開設房間",
+			Text:  "請點擊開始設定",
 			Actions: []messaging_api.ActionInterface{
 				messaging_api.UriAction{Label: "開始設定", Uri: "https://liff.line.me/" + liffID},
 			},
